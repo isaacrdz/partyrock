538eaf5f5d0e976cb851f31e317fad280758a9e1
isaacrdz <zack_cpp@hotmail.com>
2017-08-28 22:43:02 -0500

    first commit

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Isaac Rodriguez on 8/20/17.
+//  Copyright © 2017 Isaac Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+
+    
+
+
+}
+
+}
---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Isaac Rodriguez on 8/28/17.
+//  Copyright © 2017 Isaac Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
+    @IBOutlet weak var videoTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+
+}
